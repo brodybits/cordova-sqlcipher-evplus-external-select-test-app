17ffc49a8d7003740e4101183688869a53a08ca8
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-03-12 16:35:33 -0500

    cordova-sqlcipher-evplus-external-select-test-app

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Cordova sqlite test app
+# cordova-sqlcipher-evplus-external-select-test-app
 
-Template to demonstrate reproducible issues with Cordova sqlite plugin (using JQuery and Bootstrap 3). May be used as a starter project.
+based on: Cordova sqlite test app
 
 **AUTHOR:** [@brodybits (Christopher J. Brody aka Chris Brody)](https://github.com/brodybits)
 
-**LICENSE:** [CC0 1.0 (public domain)](https://creativecommons.org/publicdomain/zero/1.0/)
+**LICENSE:** GPL v3
 
-**NOTE:** This project includes JQuery (3.3.1) and Bootstrap 3 (3.3.7) under the MIT license. Otherwise there is no code copied from other sources.
+**NOTE:** This project includes JQuery (3.3.1) and Bootstrap 3 (3.3.7) under the MIT license.
 
 **IMPORTANT:** Whitelist and intent items are omitted from this test app.
 
@@ -14,65 +14,4 @@ Template to demonstrate reproducible issues with Cordova sqlite plugin (using JQ
 
 - Bootstrap (3.3.7) - included (MIT license)
 - JQuery (3.3.1) - included (MIT license)
-- `cordova-plugin-dialogs` - specified in `config.xml`
-- Cordova sqlite plugin - please add the desired version from the Cordova CLI as described below
-
-IMPORTANT NOTE: `cordova-plugin-dialogs` was added using the `--save` flag to ensure that this plugin would be tracked in `config.xml` for automatic installation. It is recommended to use the `--save` flag on Cordova pre-7.0 to track any other plugins added in `config.xml` (this is automatic starting with Cordova `7.0`). It is NOT recommended to commit `plugins` or `platforms` directories to git or any other form of source code control.
-
-Additional note: `cordova-plugin-dialogs` does not currently support macOS ("osx"). As a workaround this project automatically uses `window.alert` if necessary.
-
-## To add another plugin
-
-```shell
-cordova plugin add my-plugin-id --save
-```
-
-## How to run
-
-1. Add the desired Cordova sqlite plugin version, for example:
-
-```shell
-cordova plugin add cordova-sqlite-storage --save
-```
-
-NOTE: `--save` flag is automatic and not needed in Cordova 7.0 and greater
-
-2. Add the desired platform(s), for example:
-
-```shell
-cordova platform add android
-```
-
-3. Do `cordova prepare` (may not always be necessary but good to be on the safe side):
-
-```shell
-cordova prepare
-```
-
-4. Run it on your mobile emulator or device, for example:
-
-```shell
-cordova run android
-```
-
-## Functionality
-
-- Upon startup: open a database and CREATE the test table
-- Alert dialog test (native alert if possible)
-- Echo test
-- Self test
-- Location reload
-- String test 1
-- String test 2 (string as a SQL parameter)
-- Show record
-- Add record
-- Add 100 records from JavaScript object after delay
-- Delete all records
-- Follow link to page 2
-- Change window.location to page 2
-
-## Multi-page test
-
-It is possible to switch between two pages using "follow link" buttons. The main page also has a button to go to page 2 by changing `window.location`. There is also a button on both pages to try `location.reload()`.
-
-The sqlite plugin should continue to work after the user changes to another page or triggers `location.reload()`.
+- multiple Cordova plugins
--- www/app.js
+++ www/app.js
@@ -3,134 +3,46 @@ var database = null;
 var nextUser = 101;
 
 function initDatabase() {
-  database = window.sqlitePlugin.openDatabase({name: 'sample.db', location: 'default'});
 
+// showMessage('start init')
+
+var permissions = cordova.plugins.permissions
+
+var externalDirectory = cordova.file.externalRootDirectory
+
+permissions.requestPermission(permissions.WRITE_EXTERNAL_STORAGE,
+function() {
+window.resolveLocalFileSystemURL(externalDirectory, function(externalDirectoryEntry) {
+	// showMessage('resolved external directory URL: ' + externalDirectoryEntry.toURL())
+
+  // database = window.sqlitePlugin.openDatabase({name: 'plain.db', androidDatabaseLocation: externalDataDirectoryEntry.toURL()});
+
+  var url =  externalDirectoryEntry.toURL()
+
+  // showMessage('db location url: ' + url)
+
+  // database = window.sqlitePlugin.openDatabase({name: 'plain.db', androidDatabaseLocation: url},
+  database = window.sqlitePlugin.openDatabase({name: 'base.sqlitedb', key: 'testKey', androidDatabaseLocation: url},
+	  function() { showMessage('db open ok from location url: ' + url)},
+	  function(e) { showMessage('open error: ' + e.message) }
+  )
+
+	// showMessage('started to open the database')
+
+}, function(e) { showMessage('file error: ' + e.message) } )
+
+}, function(e) { showMessage('permissions error: ' + e.message) })
+
+}
+
+function selectTest() {
   database.transaction(function(transaction) {
-    transaction.executeSql('CREATE TABLE SampleTable (name, score)');
-  });
-}
-
-function echoTest() {
-  window.sqlitePlugin.echoTest(function() {
-    showMessage('Echo test OK');
-  }, function(error) {
-    showMessage('Echo test ERROR: ' + error.message);
-  });
-}
-
-function selfTest() {
-  window.sqlitePlugin.selfTest(function() {
-    showMessage('Self test OK');
-  }, function(error) {
-    showMessage('Self test ERROR: ' + error.message);
-  });
-}
-
-function reload() {
-  location.reload();
-}
-
-function stringTest1() {
-  database.transaction(function(transaction) {
-    transaction.executeSql("SELECT upper('Test string') AS upperText", [], function(ignored, resultSet) {
-      showMessage('Got upperText result (ALL CAPS): ' + resultSet.rows.item(0).upperText);
+    transaction.executeSql('SELECT testString FROM testTable', [], function(ignored, resultSet) {
+      showMessage('testString: ' + resultSet.rows.item(0).testString);
     });
   }, function(error) {
     showMessage('SELECT count error: ' + error.message);
   });
-}
-
-function stringTest2() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('SELECT upper(?) AS upperText', ['Test string'], function(ignored, resultSet) {
-      showMessage('Got upperText result (ALL CAPS): ' + resultSet.rows.item(0).upperText);
-    });
-  }, function(error) {
-    showMessage('SELECT count error: ' + error.message);
-  });
-}
-
-function showCount() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('SELECT count(*) AS recordCount FROM SampleTable', [], function(ignored, resultSet) {
-      showMessage('RECORD COUNT: ' + resultSet.rows.item(0).recordCount);
-    });
-  }, function(error) {
-    showMessage('SELECT count error: ' + error.message);
-  });
-}
-
-function addRecord() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('INSERT INTO SampleTable VALUES (?,?)', ['User '+nextUser, nextUser]);
-  }, function(error) {
-    showMessage('INSERT error: ' + error.message);
-  }, function() {
-    showMessage('INSERT OK');
-    ++nextUser;
-  });
-}
-
-function addJSONRecordsAfterDelay() {
-  function getJSONObjectArray() {
-    var COUNT = 100;
-    var myArray = [];
-
-    for (var i=0; i<COUNT; ++i) {
-      myArray.push({name: 'User '+nextUser, score: nextUser});
-      ++nextUser;
-    }
-
-    return myArray;
-  }
-
-  function getJSONAfterDelay() {
-    var MY_DELAY = 150;
-
-    var d = $.Deferred();
-
-    setTimeout(function() {
-      d.resolve(getJSONObjectArray());
-    }, MY_DELAY);
-
-    return $.when(d);
-  }
-
-  // NOTE: This is similar to the case when an application
-  // fetches the data over AJAX to populate the database.
-  // IMPORTANT: The application MUST get the data before
-  // starting the transaction.
-  getJSONAfterDelay().then(function(jsonObjectArray) {
-    database.transaction(function(transaction) {
-      $.each(jsonObjectArray, function(index, recordValue) {
-        transaction.executeSql('INSERT INTO SampleTable VALUES (?,?)',
-          [recordValue.name, recordValue.score]);
-      });
-    }, function(error) {
-      showMessage('ADD records after delay ERROR');
-    }, function() {
-      showMessage('ADD 100 records after delay OK');
-    });
-  });
-}
-
-function deleteRecords() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('DELETE FROM SampleTable');
-  }, function(error) {
-    showMessage('DELETE error: ' + error.message);
-  }, function() {
-    showMessage('DELETE OK');
-    ++nextUser;
-  });
-}
-
-function alertTest() {
-  showMessage('Alert test message');
-}
-
-function goToPage2() {
-  window.location = "page2.html";
 }
 
 function showMessage(message) {
@@ -140,17 +52,7 @@ function showMessage(message) {
 }
 
 document.addEventListener('deviceready', function() {
-  $('#alert-test').click(alertTest);
-  $('#echo-test').click(echoTest);
-  $('#self-test').click(selfTest);
-  $('#reload').click(reload);
-  $('#string-test-1').click(stringTest1);
-  $('#string-test-2').click(stringTest2);
-  $('#show-count').click(showCount);
-  $('#add-record').click(addRecord);
-  $('#add-json-records-after-delay').click(addJSONRecordsAfterDelay);
-  $('#delete-records').click(deleteRecords);
-  $('#location-page2').click(goToPage2);
+  $('#select-test').click(selectTest);
 
   initDatabase();
 });
